6d1f35a71879eb0de0ffb844982523c28bb88c2a
dmkk3r <dominik.koerber.95@gmail.com>
2025-04-09 21:39:35 +0200

    Increase health check timeout from 30 to 60 seconds

--- src/greenguard/web/Store/Hub.cs
+++ src/greenguard/web/Store/Hub.cs
@@ -7,5 +7,5 @@ public class Hub
     public string? IpAddress { get; set; }
     public DeviceId DeviceId { get; set; }
     public DateTime? LastHealthCheck { get; set; }
-    public bool IsOnline => LastHealthCheck != null && (DateTime.UtcNow - LastHealthCheck.Value).TotalSeconds < 30;
+    public bool IsOnline => LastHealthCheck != null && (DateTime.UtcNow - LastHealthCheck.Value).TotalSeconds < 60;
 }
